7c9c20ead4340ab5c3a407a55fa0035965422987
Agarwalla <pagarwa@lowes.com>
2019-07-27 21:23:27 +0530

    '1'

--- productcatalogapp/target/classes/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `product` (`id`, `name`, `category`, `price`) VALUES (121845, 'IPhone 6S', 'Mobile', 10000);
-INSERT INTO `product` (`id`, `name`, `category`, `price`) VALUES (254646, 'Samsung Galaxy', 'Mobile', 2000);
-INSERT INTO `product` (`id`, `name`, `category`, `price`) VALUES (336465, 'Lenovo', 'Laptop', 3000);
-INSERT INTO `product` (`id`, `name`, `category`, `price`) VALUES (489000, 'LG', 'Television', 50000);
-
-INSERT INTO `product` (`id`, `name`, `category`, `price`) VALUES (356485, 'MI', 'Television', 50000);
-INSERT INTO `product` (`id`, `name`, `category`, `price`) VALUES (121213, 'HP', 'Printer', 6000);
-INSERT INTO `product` (`id`, `name`, `category`, `price`) VALUES (909876, 'Orient', 'AirCooler', 8000);
-INSERT INTO `product` (`id`, `name`, `category`, `price`) VALUES (456743, 'Amazon', 'Tablet', 18000);
--- productcatalogapp/target/classes/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `product` (
-	`id` INT(11) NOT NULL,
-	`name` VARCHAR(50) NULL DEFAULT NULL,
-	`category` VARCHAR(50) NULL DEFAULT NULL,
-	`price` int(20) NOT NULL,
-	 PRIMARY KEY (`id`)
-);
