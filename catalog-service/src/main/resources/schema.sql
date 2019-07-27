312619cfdd443b796d8e4e6977b4a141238bf1c2
Agarwalla <pagarwa@lowes.com>
2019-07-27 21:20:34 +0530

    updated

---
+++ catalog-service/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `product` (
+	`id` INT(11) NOT NULL,
+	`name` VARCHAR(50) NULL DEFAULT NULL,
+	`category` VARCHAR(50) NULL DEFAULT NULL,
+	`price` int(20) NOT NULL,
+	 PRIMARY KEY (`id`)
+);
